b60ff4473373dff8e67f77e7e810aa5f6d80635d
Yurii Sahanovskyi <yuriisahanovskyi@gmail.com>
2024-11-01 13:18:11 +0200

    Test coverage check GitHub action for coverage Temporary integration test

---
+++ src/test/java/com/example/cosmocatsintergalacticmarketplace/TemporaryIT.java
@@ -0,0 +1,15 @@
+package com.example.cosmocatsintergalacticmarketplace;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TemporaryIT {
+
+    @Test
+    void temporaryTest() {
+        // This test will always pass
+        assertTrue(true);
+    }
+}
+
