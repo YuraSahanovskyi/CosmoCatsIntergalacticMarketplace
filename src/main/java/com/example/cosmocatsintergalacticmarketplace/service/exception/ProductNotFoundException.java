1595970dbdeb44de99c2700324e1978d63643126
Yura Sahanovskyi <yuriisahanovskyi@gmail.com>
2024-12-04 20:22:39 +0000

    Renaming wrong parameters

--- src/main/java/com/example/cosmocatsintergalacticmarketplace/service/exception/ProductNotFoundException.java
+++ src/main/java/com/example/cosmocatsintergalacticmarketplace/service/exception/ProductNotFoundException.java
@@ -2,7 +2,7 @@ package com.example.cosmocatsintergalacticmarketplace.service.exception;
 
 public class ProductNotFoundException extends RuntimeException {
     private static final String DEFAULT_MESSAGE = "Product with id %s not found";
-    public ProductNotFoundException(Long customerId) {
-        super(String.format(DEFAULT_MESSAGE, customerId));
+    public ProductNotFoundException(Long productId) {
+        super(String.format(DEFAULT_MESSAGE, productId));
     }
 }
