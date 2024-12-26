150343779cfcf1432744df4cb3c5138f03a04a8e
YuraSahanovskyi <yuriisahanovskyi@gmail.com>
2024-12-26 14:45:09 +0200

    Rename messages in exceptions

--- src/main/java/com/example/cosmocatsintergalacticmarketplace/service/exception/CategoryConflictException.java
+++ src/main/java/com/example/cosmocatsintergalacticmarketplace/service/exception/CategoryConflictException.java
@@ -1,8 +1,8 @@
 package com.example.cosmocatsintergalacticmarketplace.service.exception;
 
 public class CategoryConflictException extends RuntimeException {
-    private static final String DEFAULT_MESSAGE = "Category with name %s already exists";
+    private static final String CATEGORY_CONFLICT_MESSAGE = "Category with name %s already exists";
     public CategoryConflictException(String categoryName) {
-        super(String.format(DEFAULT_MESSAGE, categoryName));
+        super(String.format(CATEGORY_CONFLICT_MESSAGE, categoryName));
     }
 }
